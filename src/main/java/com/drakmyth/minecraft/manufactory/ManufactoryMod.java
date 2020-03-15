8cbeda2f13a5d04f6f79ab1a518284aac965a163
Drakmyth <shaun.hamman@gmail.com>
2020-03-14 19:43:12 -0700

    Initial Commit

---
+++ src/main/java/com/drakmyth/minecraft/manufactory/ManufactoryMod.java
@@ -0,0 +1,37 @@
+/*
+ *  SPDX-License-Identifier: LGPL-3.0-only
+ *  Copyright (c) 2020 Drakmyth. All rights reserved.
+ */
+
+package com.drakmyth.minecraft.manufactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ManufactoryMod {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ManufactoryMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO from Preinit");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+}
