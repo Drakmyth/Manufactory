2cec49e99b7bfda326c8a5fd7c9237672247134c
Drakmyth <shaun.hamman@gmail.com>
2020-10-18 21:27:51 -0700

    Latex Collector can only be placed on the sides of logs

--- src/main/java/com/drakmyth/minecraft/manufactory/blocks/LatexCollectorBlock.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/blocks/LatexCollectorBlock.java
@@ -7,11 +7,18 @@ package com.drakmyth.minecraft.manufactory.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Direction.Plane;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 
 public class LatexCollectorBlock extends Block {
+    public static final DirectionProperty HORIZONTAL_FACING = HorizontalBlock.HORIZONTAL_FACING;
 
     public LatexCollectorBlock(Properties properties) {
         super(properties);
@@ -19,11 +26,24 @@ public class LatexCollectorBlock extends Block {
 
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return Plane.HORIZONTAL.test(context.getFace()) ? getDefaultState() : null;
+        final Direction face = context.getFace();
+        if (!isFaceHorizontal(face) || !isBlockLog(context.getWorld(), context.getPos().offset(face.getOpposite()))) {
+            return null;
+        }
+
+        return this.getDefaultState().with(HORIZONTAL_FACING, face.getOpposite());
+    }
+
+    private boolean isFaceHorizontal(Direction direction) {
+        return Plane.HORIZONTAL.test(direction);
+    }
+
+    private boolean isBlockLog(IWorldReader world, BlockPos pos) {
+        return world.getBlockState(pos).func_235714_a_(BlockTags.LOGS);
     }
 
     @Override
     protected void fillStateContainer(Builder<Block, BlockState> builder) {
-        super.fillStateContainer(builder);
+        builder.add(HORIZONTAL_FACING);
     }
 }
