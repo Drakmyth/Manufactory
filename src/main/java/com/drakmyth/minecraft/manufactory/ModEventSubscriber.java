b069ebdcc2b13d296da14a09cedf7e031742bb62
Drakmyth <shaun.hamman@gmail.com>
2020-10-23 10:44:35 -0700

    Migrate recipes to data generators

--- src/main/java/com/drakmyth/minecraft/manufactory/ModEventSubscriber.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/ModEventSubscriber.java
@@ -5,17 +5,20 @@
 
 package com.drakmyth.minecraft.manufactory;
 
+import com.drakmyth.minecraft.manufactory.datagen.ModRecipeProvider;
 import com.drakmyth.minecraft.manufactory.init.ModBlocks;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.Block;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @EventBusSubscriber(modid = Reference.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
@@ -36,5 +39,11 @@ public final class ModEventSubscriber {
                 blockItem.setRegistryName(block.getRegistryName());
                 registry.register(blockItem);
             });
+    }
+
+    @SubscribeEvent
+    public static void gatherData(final GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        generator.addProvider(new ModRecipeProvider(generator));
     }
 }
