0fe0c71aeb10db7f2af6b4f58209d771f6779445
Drakmyth <shaun.hamman@gmail.com>
2020-10-24 11:58:23 -0700

    Make Latex Collector visually full when Fill Status is FILLING

--- src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModBlockStateProvider.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModBlockStateProvider.java
@@ -68,11 +68,17 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     private ModelFile generatePartialLatexCollectorFillingModel() {
         BlockModelBuilder builder = models().getBuilder("latex_collector_filling");
+        // latex_surface
+        builder.element().from(6, 3, 1).to(10, 4, 5).face(Direction.UP).texture("#latex").end().end();
+        // latex_stream
+        builder.element().from(7, 5, 0).to(9, 12, 2).allFaces((dir, face) -> face.texture("#latex")).end();
+        builder.texture("latex", "minecraft:block/quartz_block_top");
         return builder;
     }
 
     private ModelFile generatePartialLatexCollectorFullModel() {
         BlockModelBuilder builder = models().getBuilder("latex_collector_full");
+        // latex_surface
         builder.element().from(6, 4, 1).to(10, 5, 5).face(Direction.UP).texture("#latex").end().end();
         builder.texture("latex", "minecraft:block/quartz_block_top");
         return builder;
@@ -84,6 +90,10 @@ public class ModBlockStateProvider extends BlockStateProvider {
         builder.part().modelFile(emptyModel).rotationY(90).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.EAST);
         builder.part().modelFile(emptyModel).rotationY(180).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.SOUTH);
         builder.part().modelFile(emptyModel).rotationY(270).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.WEST);
+        builder.part().modelFile(fillingModel).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.NORTH).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FILLING);
+        builder.part().modelFile(fillingModel).rotationY(90).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.EAST).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FILLING);
+        builder.part().modelFile(fillingModel).rotationY(180).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.SOUTH).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FILLING);
+        builder.part().modelFile(fillingModel).rotationY(270).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.WEST).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FILLING);
         builder.part().modelFile(fullModel).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.NORTH).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
         builder.part().modelFile(fullModel).rotationY(90).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.EAST).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
         builder.part().modelFile(fullModel).rotationY(180).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.SOUTH).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
