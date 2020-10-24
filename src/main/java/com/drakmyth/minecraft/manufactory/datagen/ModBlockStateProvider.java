a3efc1f9a315b52fbeb68b02634e00bec58a480b
Drakmyth <shaun.hamman@gmail.com>
2020-10-24 10:58:51 -0700

    Make Latex Collector visually full when Fill Status is FULL

--- src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModBlockStateProvider.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModBlockStateProvider.java
@@ -17,6 +17,7 @@ import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class ModBlockStateProvider extends BlockStateProvider {
@@ -32,8 +33,8 @@ public class ModBlockStateProvider extends BlockStateProvider {
         itemModels().getBuilder("amber_block").parent(amberBlockModel);
 
         ModelFile latexCollectorEmptyModel = generateLatexCollectorEmptyModel();
-        ModelFile latexCollectorFillingModel = generateLatexCollectorFillingModel(latexCollectorEmptyModel);
-        ModelFile latexCollectorFullModel = generateLatexCollectorFullModel(latexCollectorEmptyModel);
+        ModelFile latexCollectorFillingModel = generatePartialLatexCollectorFillingModel();
+        ModelFile latexCollectorFullModel = generatePartialLatexCollectorFullModel();
         generateLatexCollectorBlockState(latexCollectorEmptyModel, latexCollectorFillingModel, latexCollectorFullModel);
 
         itemModels().withExistingParent("item/latex_collector", "item/handheld").texture("layer0", "manufactory:item/latex_collector");
@@ -51,56 +52,41 @@ public class ModBlockStateProvider extends BlockStateProvider {
         BlockModelBuilder builder = models().getBuilder("latex_collector");
 
         // bottom
-        builder.element().from(6, 2, 1).to(10, 3, 5).allFaces((dir, face) -> face.texture("#0")).end();
+        builder.element().from(6, 2, 1).to(10, 3, 5).allFaces((dir, face) -> face.texture("#collector")).end();
         // north_side
-        builder.element().from(5, 3, 0).to(11, 5, 1).allFaces((dir, face) -> face.texture("#0")).end();
+        builder.element().from(5, 3, 0).to(11, 5, 1).allFaces((dir, face) -> face.texture("#collector")).end();
         // south_side
-        builder.element().from(5, 3, 5).to(11, 5, 6).allFaces((dir, face) -> face.texture("#0")).end();
+        builder.element().from(5, 3, 5).to(11, 5, 6).allFaces((dir, face) -> face.texture("#collector")).end();
         // west_side
-        builder.element().from(5, 3, 1).to(6, 5, 5).allFaces((dir, face) -> face.texture("#0")).end();
+        builder.element().from(5, 3, 1).to(6, 5, 5).allFaces((dir, face) -> face.texture("#collector")).end();
         // east_side
-        builder.element().from(10, 3, 1).to(11, 5, 5).allFaces((dir, face) -> face.texture("#0")).end();
-        builder.texture("0", "minecraft:block/dirt");
+        builder.element().from(10, 3, 1).to(11, 5, 5).allFaces((dir, face) -> face.texture("#collector")).end();
+        builder.texture("collector", "minecraft:block/dirt");
         builder.texture("particle", "minecraft:block/dirt");
         return builder;
     }
 
-    private ModelFile generateLatexCollectorFillingModel(ModelFile emptyModel) {
+    private ModelFile generatePartialLatexCollectorFillingModel() {
         BlockModelBuilder builder = models().getBuilder("latex_collector_filling");
-        builder.parent(emptyModel);
-        // builder.element().from(6, 2, 1).to(10, 3, 5).allFaces((dir, face) -> face.texture("#0")).end();
         return builder;
     }
 
-    private ModelFile generateLatexCollectorFullModel(ModelFile emptyModel) {
+    private ModelFile generatePartialLatexCollectorFullModel() {
         BlockModelBuilder builder = models().getBuilder("latex_collector_full");
-        builder.parent(emptyModel);
-        // builder.element().from(6, 2, 1).to(10, 3, 5).allFaces((dir, face) -> face.texture("#0")).end();
+        builder.element().from(6, 4, 1).to(10, 5, 5).face(Direction.UP).texture("#latex").end().end();
+        builder.texture("latex", "minecraft:block/quartz_block_top");
         return builder;
     }
 
     private void generateLatexCollectorBlockState(ModelFile emptyModel, ModelFile fillingModel, ModelFile fullModel) {
-        getVariantBuilder(ModBlocks.LATEX_COLLECTOR.get())
-            .forAllStatesExcept(state -> {
-                Direction dir = state.get(LatexCollectorBlock.HORIZONTAL_FACING);
-                LatexCollectorBlock.FillStatus fillStatus = state.get(LatexCollectorBlock.FILL_STATUS);
-                ModelFile stateModel;
-                switch (fillStatus) {
-                    default:
-                    case EMPTY:
-                        stateModel = emptyModel;
-                        break;
-                    case FILLING:
-                        stateModel = fillingModel;
-                        break;
-                    case FULL:
-                        stateModel = fullModel;
-                        break;
-                }
-                return ConfiguredModel.builder()
-                    .modelFile(stateModel)
-                    .rotationY((int)dir.rotateY().rotateY().getHorizontalAngle())
-                    .build();
-            }, LatexCollectorBlock.WATERLOGGED);
+        MultiPartBlockStateBuilder builder = getMultipartBuilder(ModBlocks.LATEX_COLLECTOR.get());
+        builder.part().modelFile(emptyModel).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.NORTH);
+        builder.part().modelFile(emptyModel).rotationY(90).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.EAST);
+        builder.part().modelFile(emptyModel).rotationY(180).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.SOUTH);
+        builder.part().modelFile(emptyModel).rotationY(270).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.WEST);
+        builder.part().modelFile(fullModel).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.NORTH).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
+        builder.part().modelFile(fullModel).rotationY(90).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.EAST).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
+        builder.part().modelFile(fullModel).rotationY(180).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.SOUTH).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
+        builder.part().modelFile(fullModel).rotationY(270).addModel().condition(LatexCollectorBlock.HORIZONTAL_FACING, Direction.WEST).condition(LatexCollectorBlock.FILL_STATUS, LatexCollectorBlock.FillStatus.FULL);
     }
 }
