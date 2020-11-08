10c8d00b82ea9ffc9aca07d4692b5131a60fe982
Drakmyth <shaun.hamman@gmail.com>
2020-11-08 13:33:44 -0800

    Changed Milling Ball Tier 0 condition to use planks tag. Replaced explicit InventoryChangeTrigger access with helper method.

--- src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModRecipeProvider.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModRecipeProvider.java
@@ -14,7 +14,6 @@ import com.drakmyth.minecraft.manufactory.init.ModBlocks;
 import com.drakmyth.minecraft.manufactory.init.ModItems;
 import com.drakmyth.minecraft.manufactory.recipes.ManufactoryRecipeBuilder;
 
-import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.data.CookingRecipeBuilder;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
@@ -42,20 +41,20 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine("aaa")
             .patternLine("aaa")
             .key('a', ModItems.AMBER.get())
-            .addCriterion("has_amber", InventoryChangeTrigger.Instance.forItems(ModItems.AMBER.get()))
+            .addCriterion("has_amber", hasItem(ModItems.AMBER.get()))
             .build(consumer);
 
         // 9 Amber = Amber Block
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.AMBER.get(), 9)
             .addIngredient(ModBlocks.AMBER_BLOCK.get())
-            .addCriterion("has_amber_block", InventoryChangeTrigger.Instance.forItems(ModBlocks.AMBER_BLOCK.get()))
+            .addCriterion("has_amber_block", hasItem(ModBlocks.AMBER_BLOCK.get()))
             .build(consumer);
 
         // Latex Collector = String + Bowl
         ShapelessRecipeBuilder.shapelessRecipe(ModBlocks.LATEX_COLLECTOR.get())
             .addIngredient(Items.STRING)
             .addIngredient(Items.BOWL)
-            .addCriterion("has_bowl", InventoryChangeTrigger.Instance.forItems(Items.BOWL))
+            .addCriterion("has_bowl", hasItem(Items.BOWL))
             .build(consumer);
 
         // Tapping Knife = 2 Iron Ingot + Stick
@@ -64,19 +63,19 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine("  s")
             .key('i', Items.IRON_INGOT)
             .key('s', Items.STICK)
-            .addCriterion("has_iron_ingot", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT))
+            .addCriterion("has_iron_ingot", hasItem(Items.IRON_INGOT))
             .build(consumer);
 
         // Rubber = Coagulated Latex
         CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(ModItems.COAGULATED_LATEX.get()), ModItems.RUBBER.get(), 0.1f, 200)
-            .addCriterion("has_coagulated_latex", InventoryChangeTrigger.Instance.forItems(ModItems.COAGULATED_LATEX.get()))
+            .addCriterion("has_coagulated_latex", hasItem(ModItems.COAGULATED_LATEX.get()))
             .build(consumer);
 
         // 3 Redstone Wire = 3 String + Redstone Dust
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.REDSTONE_WIRE.get(), 3)
             .addIngredient(Items.REDSTONE)
             .addIngredient(Items.STRING, 3)
-            .addCriterion("has_string", InventoryChangeTrigger.Instance.forItems(Items.STRING))
+            .addCriterion("has_string", hasItem(Items.STRING))
             .build(consumer);
 
         // Coupling = 4 Iron Ingot
@@ -85,7 +84,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine("i i")
             .patternLine(" i ")
             .key('i', Items.IRON_INGOT)
-            .addCriterion("has_iron_ingot", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT))
+            .addCriterion("has_iron_ingot", hasItem(Items.IRON_INGOT))
             .build(consumer);
 
         // Wrench = 4 Iron Ingot
@@ -94,7 +93,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine(" i ")
             .patternLine(" i ")
             .key('i', Items.IRON_INGOT)
-            .addCriterion("has_iron_ingot", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT))
+            .addCriterion("has_iron_ingot", hasItem(Items.IRON_INGOT))
             .build(consumer);
 
         // Motor Tier 0 = Coupling + Redstone Dust + 3 Iron Ingot
@@ -105,28 +104,28 @@ public class ModRecipeProvider extends RecipeProvider {
             .key('i', Items.IRON_INGOT)
             .key('c', ModItems.COUPLING.get())
             .key('r', Items.REDSTONE)
-            .addCriterion("has_redstone", InventoryChangeTrigger.Instance.forItems(Items.REDSTONE))
+            .addCriterion("has_redstone", hasItem(Items.REDSTONE))
             .build(consumer);
 
         // Motor Tier 1 = Motor Tier 0 + Gold Ingot
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MOTOR_TIER1.get())
             .addIngredient(ModItems.MOTOR_TIER0.get())
             .addIngredient(Items.GOLD_INGOT)
-            .addCriterion("has_motor_tier_0", InventoryChangeTrigger.Instance.forItems(ModItems.MOTOR_TIER0.get()))
+            .addCriterion("has_motor_tier_0", hasItem(ModItems.MOTOR_TIER0.get()))
             .build(consumer);
 
         // Motor Tier 2 = Motor Tier 1 + Diamond
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MOTOR_TIER2.get())
             .addIngredient(ModItems.MOTOR_TIER1.get())
             .addIngredient(Items.DIAMOND)
-            .addCriterion("has_motor_tier_1", InventoryChangeTrigger.Instance.forItems(ModItems.MOTOR_TIER1.get()))
+            .addCriterion("has_motor_tier_1", hasItem(ModItems.MOTOR_TIER1.get()))
             .build(consumer);
 
         // Motor Tier 3 = Motor Tier 2 + Netherite Ingot
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MOTOR_TIER3.get())
             .addIngredient(ModItems.MOTOR_TIER2.get())
             .addIngredient(Items.NETHERITE_INGOT)
-            .addCriterion("has_motor_tier_2", InventoryChangeTrigger.Instance.forItems(ModItems.MOTOR_TIER2.get()))
+            .addCriterion("has_motor_tier_2", hasItem(ModItems.MOTOR_TIER2.get()))
             .build(consumer);
 
         // Battery = Redstone Wire + Nether Quartz + Redstone Dust
@@ -137,7 +136,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .key('w', ModItems.REDSTONE_WIRE.get())
             .key('n', Items.QUARTZ)
             .key('r', Items.REDSTONE)
-            .addCriterion("has_nether_quartz", InventoryChangeTrigger.Instance.forItems(Items.QUARTZ))
+            .addCriterion("has_nether_quartz", hasItem(Items.QUARTZ))
             .build(consumer);
 
         // Power Socket = 3 Redstone Wire + 6 Coagulated Latex
@@ -147,7 +146,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine("lwl")
             .key('w', ModItems.REDSTONE_WIRE.get())
             .key('l', ModItems.COAGULATED_LATEX.get())
-            .addCriterion("has_redstone_wire", InventoryChangeTrigger.Instance.forItems(ModItems.REDSTONE_WIRE.get()))
+            .addCriterion("has_redstone_wire", hasItem(ModItems.REDSTONE_WIRE.get()))
             .build(consumer);
 
         // Grinder Wheel Tier 0 = Coupling + 4 Planks
@@ -157,7 +156,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine(" p ")
             .key('c', ModItems.COUPLING.get())
             .key('p', ItemTags.PLANKS)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // Grinder Wheel Tier 1 = Coupling + 4 Cobblestone
@@ -167,7 +166,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine(" s ")
             .key('c', ModItems.COUPLING.get())
             .key('s', ItemTags.STONE_CRAFTING_MATERIALS)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // Grinder Wheel Tier 2 = Coupling + 4 Iron Ingot
@@ -177,7 +176,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine(" i ")
             .key('c', ModItems.COUPLING.get())
             .key('i', Items.IRON_INGOT)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // Grinder Wheel Tier 3 = Coupling + 4 Diamond
@@ -187,7 +186,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine(" d ")
             .key('c', ModItems.COUPLING.get())
             .key('d', Items.DIAMOND)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // Grinder Wheel Tier 4 = Coupling + 4 Netherite Ingot
@@ -197,7 +196,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine(" n ")
             .key('c', ModItems.COUPLING.get())
             .key('n', Items.NETHERITE_INGOT)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // 3 Power Cable = 3 Rubber + 3 Redstone Wire
@@ -206,7 +205,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine("www")
             .key('r', ModItems.RUBBER.get())
             .key('w', ModItems.REDSTONE_WIRE.get())
-            .addCriterion("has_rubber", InventoryChangeTrigger.Instance.forItems(ModItems.RUBBER.get()))
+            .addCriterion("has_rubber", hasItem(ModItems.RUBBER.get()))
             .build(consumer);
 
         // Solar Panel = 3 Daylight Detector + 2 Nether Quartz + Redstone Dust + 2 Wooden Slab
@@ -219,7 +218,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .key('r', Items.REDSTONE)
             .key('w', ItemTags.WOODEN_SLABS)
             .key('p', ModBlocks.POWER_CABLE.get())
-            .addCriterion("has_daylight_detector", InventoryChangeTrigger.Instance.forItems(Items.DAYLIGHT_DETECTOR))
+            .addCriterion("has_daylight_detector", hasItem(Items.DAYLIGHT_DETECTOR))
             .build(consumer);
 
         // Grinder = 2 Coupling + 2 Redstone Wire + 2 Stone + Power Cable
@@ -231,7 +230,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .key('w', ModItems.REDSTONE_WIRE.get())
             .key('p', ModBlocks.POWER_CABLE.get())
             .key('s', Items.STONE)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // Ball Mill = 1 Coupling + 3 Redstone Wire + 4 Stone + Power Cable
@@ -243,7 +242,7 @@ public class ModRecipeProvider extends RecipeProvider {
             .key('w', ModItems.REDSTONE_WIRE.get())
             .key('p', ModBlocks.POWER_CABLE.get())
             .key('s', Items.STONE)
-            .addCriterion("has_coupling", InventoryChangeTrigger.Instance.forItems(ModItems.COUPLING.get()))
+            .addCriterion("has_coupling", hasItem(ModItems.COUPLING.get()))
             .build(consumer);
 
         // 4 Milling Ball Tier 0 = 4 Planks
@@ -252,35 +251,35 @@ public class ModRecipeProvider extends RecipeProvider {
             .patternLine("p p")
             .patternLine(" p ")
             .key('p', ItemTags.PLANKS)
-            .addCriterion("has_wood_planks", InventoryChangeTrigger.Instance.forItems(Items.OAK_PLANKS))
+            .addCriterion("has_wood_planks", hasItem(ItemTags.PLANKS))
             .build(consumer);
 
         // Milling Ball Tier 1 = Milling Ball Tier 0 + Cobblestone
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MILLING_BALL_TIER1.get())
             .addIngredient(ModItems.MILLING_BALL_TIER0.get())
             .addIngredient(Items.COBBLESTONE)
-            .addCriterion("has_milling_ball_tier0", InventoryChangeTrigger.Instance.forItems(ModItems.MILLING_BALL_TIER0.get()))
+            .addCriterion("has_milling_ball_tier0", hasItem(ModItems.MILLING_BALL_TIER0.get()))
             .build(consumer);
 
         // Milling Ball Tier 2 = Milling Ball Tier 1 + Iron Ingot
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MILLING_BALL_TIER2.get())
             .addIngredient(ModItems.MILLING_BALL_TIER1.get())
             .addIngredient(Items.IRON_INGOT)
-            .addCriterion("has_milling_ball_tier1", InventoryChangeTrigger.Instance.forItems(ModItems.MILLING_BALL_TIER1.get()))
+            .addCriterion("has_milling_ball_tier1", hasItem(ModItems.MILLING_BALL_TIER1.get()))
             .build(consumer);
 
         // Milling Ball Tier 3 = Milling Ball Tier 2 + Diamond
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MILLING_BALL_TIER3.get())
             .addIngredient(ModItems.MILLING_BALL_TIER2.get())
             .addIngredient(Items.DIAMOND)
-            .addCriterion("has_milling_ball_tier2", InventoryChangeTrigger.Instance.forItems(ModItems.MILLING_BALL_TIER2.get()))
+            .addCriterion("has_milling_ball_tier2", hasItem(ModItems.MILLING_BALL_TIER2.get()))
             .build(consumer);
 
         // Milling Ball Tier 4 = Milling Ball Tier 3 + Netherite Ingot
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.MILLING_BALL_TIER4.get())
             .addIngredient(ModItems.MILLING_BALL_TIER3.get())
             .addIngredient(Items.NETHERITE_INGOT)
-            .addCriterion("has_milling_ball_tier3", InventoryChangeTrigger.Instance.forItems(ModItems.MILLING_BALL_TIER3.get()))
+            .addCriterion("has_milling_ball_tier3", hasItem(ModItems.MILLING_BALL_TIER3.get()))
             .build(consumer);
 
         List<OreProcessingRecipeData> grinderOres = Arrays.asList(
@@ -304,12 +303,12 @@ public class ModRecipeProvider extends RecipeProvider {
             ManufactoryRecipeBuilder.grinderRecipe(Ingredient.fromItems(data.getInput()), data.getOutput())
                 .withExtraChance(0.3f, data.getExtraAmounts())
                 .withTierRequired(data.getTier().getHarvestLevel())
-                .addCriterion(String.format("has_%s", inputName), InventoryChangeTrigger.Instance.forItems(data.getInput()))
+                .addCriterion(String.format("has_%s", inputName), hasItem(data.getInput()))
                 .build(consumer);
 
             // Ground Ore (Rough) -> Ingot
             CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(data.getOutput()), data.getProcessed(), 0.2f, 200)
-                .addCriterion(String.format("has_%s", outputName), InventoryChangeTrigger.Instance.forItems(data.getOutput()))
+                .addCriterion(String.format("has_%s", outputName), hasItem(data.getOutput()))
                 .build(consumer, String.format("%s:%s_from_ground_ore_rough", Reference.MOD_ID, processedName));
         });
 
@@ -317,7 +316,7 @@ public class ModRecipeProvider extends RecipeProvider {
         ManufactoryRecipeBuilder.grinderRecipe(Ingredient.fromItems(Items.NETHER_GOLD_ORE), ModItems.GROUND_GOLD_ORE_ROUGH.get())
             .withExtraChance(0.3f, 1)
             .withTierRequired(ItemTier.WOOD.getHarvestLevel())
-            .addCriterion("has_nether_gold_ore", InventoryChangeTrigger.Instance.forItems(Items.NETHER_GOLD_ORE))
+            .addCriterion("has_nether_gold_ore", hasItem(Items.NETHER_GOLD_ORE))
             .build(consumer, String.format("%s:ground_gold_ore_rough_from_nether_gold_ore", Reference.MOD_ID));
 
         List<OreProcessingRecipeData> ballMillOres = Arrays.asList(
@@ -341,12 +340,12 @@ public class ModRecipeProvider extends RecipeProvider {
             ManufactoryRecipeBuilder.ballMillRecipe(Ingredient.fromItems(data.getInput()), data.getOutput())
                 .withExtraChance(0.54f, data.getExtraAmounts())
                 .withTierRequired(data.getTier().getHarvestLevel())
-                .addCriterion(String.format("has_%s", inputName), InventoryChangeTrigger.Instance.forItems(data.getInput()))
+                .addCriterion(String.format("has_%s", inputName), hasItem(data.getInput()))
                 .build(consumer);
 
             // Ground Ore (Fine) -> Ingot
             CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(data.getOutput()), data.getProcessed(), 0.2f, 200)
-                .addCriterion(String.format("has_%s", outputName), InventoryChangeTrigger.Instance.forItems(data.getOutput()))
+                .addCriterion(String.format("has_%s", outputName), hasItem(data.getOutput()))
                 .build(consumer, String.format("%s:%s_from_ground_ore_fine", Reference.MOD_ID, processedName));
         });
     }
