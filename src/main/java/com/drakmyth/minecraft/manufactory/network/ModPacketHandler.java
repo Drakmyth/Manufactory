5ce496e62edea8d9094cce47c8c9d7eac4da7e04
Drakmyth <shaun.hamman@gmail.com>
2020-11-09 10:26:36 -0800

    Added logging to network classes

--- src/main/java/com/drakmyth/minecraft/manufactory/network/ModPacketHandler.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/network/ModPacketHandler.java
@@ -7,6 +7,9 @@ package com.drakmyth.minecraft.manufactory.network;
 
 import com.drakmyth.minecraft.manufactory.Reference;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkRegistry;
@@ -14,6 +17,8 @@ import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class ModPacketHandler {
     private static final String PROTOCOL_VERSION = "1";
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static final SimpleChannel INSTANCE = NetworkRegistry.ChannelBuilder
             .named(new ResourceLocation(Reference.MOD_ID, "main"))
             .networkProtocolVersion(() -> PROTOCOL_VERSION)
@@ -23,6 +28,7 @@ public class ModPacketHandler {
 
     public static void registerMessages() {
         int messageId = 0;
+        LOGGER.debug("Registering MachineProgressPacket with id %d...", messageId);
         INSTANCE.messageBuilder(MachineProgressPacket.class, messageId++, NetworkDirection.PLAY_TO_CLIENT)
                 .encoder(MachineProgressPacket::encode)
                 .decoder(MachineProgressPacket::decode)
