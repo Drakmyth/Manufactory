5913e398d24c74c5e456adf71f3f06b22f688775
Drakmyth <shaun.hamman@gmail.com>
2020-11-09 09:58:07 -0800

    Add logging to gui classes

--- src/main/java/com/drakmyth/minecraft/manufactory/gui/BallMillUpgradeGui.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/gui/BallMillUpgradeGui.java
@@ -9,23 +9,29 @@ import com.drakmyth.minecraft.manufactory.Reference;
 import com.drakmyth.minecraft.manufactory.containers.BallMillUpgradeContainer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
 public class BallMillUpgradeGui extends ContainerScreen<BallMillUpgradeContainer> {
-
     private static final ResourceLocation GUI_TEXTURE = new ResourceLocation(Reference.MOD_ID, "textures/gui/ball_mill_upgrade.png");
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public BallMillUpgradeGui(BallMillUpgradeContainer screenContainer, PlayerInventory inv, ITextComponent name) {
         super(screenContainer, inv, name);
     }
 
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        LOGGER.trace("Rendering Ball Mill Upgrade gui...");
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
         this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
+        LOGGER.trace("Ball Mill Upgrade gui render complete");
      }
 
     @Override
