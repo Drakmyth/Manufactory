58c90aaeaddeed97a92bfd41c23c8127340556e5
Drakmyth <shaun.hamman@gmail.com>
2021-09-29 11:18:41 -0700

    Remove unused using

--- src/main/java/com/drakmyth/minecraft/manufactory/containers/RockDrillUpgradeContainerProvider.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/containers/RockDrillUpgradeContainerProvider.java
@@ -15,7 +15,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.items.wrapper.InvWrapper;
