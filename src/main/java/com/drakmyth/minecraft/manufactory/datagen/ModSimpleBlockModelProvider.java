714f45807bfe4d4b07c9d2862256df538311fee0
Drakmyth <shaun.hamman@gmail.com>
2026-07-21 19:26:40 -0400

    Modernization Phase 11 - Fluid DataGen

--- src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModSimpleBlockModelProvider.java
+++ src/main/java/com/drakmyth/minecraft/manufactory/datagen/ModSimpleBlockModelProvider.java
@@ -6,8 +6,12 @@ import java.util.stream.Stream;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.core.Holder;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
@@ -22,6 +26,16 @@ public class ModSimpleBlockModelProvider extends ModelProvider {
         blockModels.createTrivialCube(ModBlocks.METALOSOL_BLOCK.get());
         blockModels.createTrivialCube(ModBlocks.MECHANITE_BLOCK.get());
         blockModels.createTrivialCube(ModBlocks.MECHANITE_PANEL.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_COAL_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_DIAMOND_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_EMERALD_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_GOLD_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_IRON_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_COPPER_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_LAPIS_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_NETHER_QUARTZ_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_REDSTONE_ORE.get());
+        createFluidModel(blockModels, ModBlocks.SLURRIED_ANCIENT_DEBRIS.get());
     }
 
     @Override
@@ -30,7 +44,17 @@ public class ModSimpleBlockModelProvider extends ModelProvider {
                 ModBlocks.AMBER_BLOCK,
                 ModBlocks.METALOSOL_BLOCK,
                 ModBlocks.MECHANITE_BLOCK,
-                ModBlocks.MECHANITE_PANEL);
+                ModBlocks.MECHANITE_PANEL,
+                ModBlocks.SLURRIED_COAL_ORE,
+                ModBlocks.SLURRIED_DIAMOND_ORE,
+                ModBlocks.SLURRIED_EMERALD_ORE,
+                ModBlocks.SLURRIED_GOLD_ORE,
+                ModBlocks.SLURRIED_IRON_ORE,
+                ModBlocks.SLURRIED_COPPER_ORE,
+                ModBlocks.SLURRIED_LAPIS_ORE,
+                ModBlocks.SLURRIED_NETHER_QUARTZ_ORE,
+                ModBlocks.SLURRIED_REDSTONE_ORE,
+                ModBlocks.SLURRIED_ANCIENT_DEBRIS);
     }
 
     @Override
@@ -45,5 +69,16 @@ public class ModSimpleBlockModelProvider extends ModelProvider {
     @Override
     public String getName() {
         return "Manufactory simple block models";
+    }
+
+    private static void createFluidModel(BlockModelGenerators blockModels, Block block) {
+        String path = block.builtInRegistryHolder().key().identifier().getPath();
+        Identifier model = ModelTemplates.PARTICLE_ONLY.create(
+                block,
+                TextureMapping.particle(new Material(Identifier.fromNamespaceAndPath(
+                        Reference.MOD_ID, "block/" + path + "_still"))),
+                blockModels.modelOutput);
+        blockModels.blockStateOutput.accept(BlockModelGenerators.createSimpleBlock(
+                block, BlockModelGenerators.plainVariant(model)));
     }
 }
